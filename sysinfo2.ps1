b86aa62d8830e34d7c0bf6be29f581f94085a677
UC-Chellaph <chellaph@mail.uc.edu>
2021-01-30 13:54:09 -0500

    Added Scripts for Sysinfo2

---
+++ sysinfo2.ps1
@@ -0,0 +1,22 @@
+﻿function getIP{
+(Get-NetIPAddress).ipv4address | Select-String "192*"
+}
+
+$IP = getIP
+$User = Get-LocalUser
+$PowershellVersion = $Host.Version.Major
+$Date = Get-Date
+
+$BODY = "This Machine's IP is $IP 
+User is $env:username
+Hostname is $env:COMPUTERNAME 
+Powershell Version $PowershellVersion 
+Today's Date is $Date"
+
+Write-Host $Body
+
+function sendMail{
+Send-MailMessage -To "chellaph@mail.uc.edu" -From "chellaph@mail.uc.edu" -
+Subject "IT3038C Windows SysInfo" -Body $BODY -SmtpServer
+smtp.google.com -port 587 -UseSSL -Credential (Get-Credential)
+}
